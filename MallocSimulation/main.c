94bc98c2cdccaf6458aeb681fbbc11b4375ca923
Omar Gudino <omfegumo6@gmail.com>
2013-09-11 11:59:35 -0700

    First Commit

---
+++ MallocSimulation/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MallocSimulation
+//
+//  Created by Omar Gudino on 9/11/13.
+//  Copyright (c) 2013 Omar Gudino. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
