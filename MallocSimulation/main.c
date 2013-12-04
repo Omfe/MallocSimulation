bd8fc0a56939d4691a9db943be4095247253e6e0
Omar Gudino <omfegumo6@gmail.com>
2013-12-04 14:25:36 -0800

    Final Commit - Simulates memory allocation

--- MallocSimulation/main.c
+++ MallocSimulation/main.c
@@ -8,11 +8,57 @@
 
 #include <stdio.h>
 
+
+
+void malloc_t(char string[], char newString[] , int lenght){
+    
+    for(int i=0; i < lenght; i++){
+        if (string[i] != '\0') {
+        newString[i] = string[i];
+        } else{
+            break;
+        }
+    }
+}
+
+void free_t(char string[], int lenght)
+{
+    for (int i = 0; i<lenght; i++) {
+        string[i] = ' ';
+    }
+}
+
 int main(int argc, const char * argv[])
 {
+    
+    printf("Favor de escribir un solo pedazo de texto que desee guardar\n");
+    
+    int i = 0;
+    char myString[100];
+    scanf("%s", myString);
+    while (myString[i] != '\0') {
+        i++;
+    }
+    char myMemory[i];
+    
+    printf("Malloc:\n");
+    malloc_t(myString, myMemory, i);
 
-    // insert code here...
-    printf("Hello, World!\n");
+    for (int j = 0; j<i; j++) {
+        printf("%c", myMemory[j]);
+    }
+    printf("\nFree:\n");
+    
+    free_t(myMemory, i);
+    
+    for (int j = 0; j<i; j++) {
+        printf("%c", myMemory[j]);
+    }
+    printf("\n");
+    
     return 0;
 }
+
+
+//entonces ingreso texto, eso se guarda en un array de chars, luego uso el metodo malloc para hacer un array de chars con un tamaño mas preciso con el contenido que ingrese y luego un metodo free para vaciar el espacio de ese malloc?
 
